78eb822c166c23bac2fc70727ecdbac3e059d83f
Tatsuya Koshimoto <e27867@jp.ibm.com>
2021-03-02 17:53:42 +0900

    new

--- sample2/deploy.sh
+++
@@ -1,9 +0,0 @@
-cd /home/tatsuya/git/project/sample2
-git pull
-cp -p Dockerfile /home/tatsuya/resource/sample2/
-cd /home/tatsuya/resource/sample2
-sudo docker build -t sample2:0.1 .
-sudo docker tag sample2:0.1 tkoshimoto/sample2:0.1
-sudo docker push tkoshimoto/sample2:0.1
-cd /home/tatsuya/git/project/sample2
-oc apply -f deployment.yaml
